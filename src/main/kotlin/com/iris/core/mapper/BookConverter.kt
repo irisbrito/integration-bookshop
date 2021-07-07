8160b34bde59bd21271d6c8a8b865a236ef85761
Iris <iris.souza@zup.com.br>
2021-07-07 16:37:32 -0300

    Refatorar BookConverter

--- src/main/kotlin/com/iris/core/mapper/BookConverter.kt
+++ src/main/kotlin/com/iris/core/mapper/BookConverter.kt
@@ -4,22 +4,30 @@ import com.iris.core.model.Book
 import com.iris.core.model.Writer
 import com.iris.entrypoint.dto.BookDto
 import com.iris.entrypoint.dto.WriterDto
+import io.reactivex.Maybe
 import kotlin.streams.toList
 
 class BookConverter {
-    fun bookDtoToBook(bookDto: BookDto) =
-        Book(bookDto.id,bookDto.name,bookDto.price,bookDto.description,
-            Writer(bookDto.id,bookDto.writer.nome,bookDto.writer.nationality))
+    companion object{
+        fun bookDtoToBook(bookDto: BookDto) =
+            Book(bookDto.id,bookDto.name,bookDto.price,bookDto.description,
+                WriterConverter.writerDtoToWriter(
+                    WriterDto(bookDto.id,bookDto.writer.nome,bookDto.writer.nationality)))
 
-    fun bookToBookDto(book: Book) =
-        BookDto(book.id,book.name,book.price,book.description,
-            WriterDto(book.writer.id,book.writer.nome,book.writer.nationality))
+        fun bookToBookDto(book: Book) =
+            BookDto(book.id,book.name,book.price,book.description,
+                WriterConverter.writerToWriterDto(
+                    Writer(book.writer.id,book.writer.nome,book.writer.nationality)))
 
-    fun listBookToListBookDto(book: List<Book>) =
-        book.stream().map { book -> BookDto(book.id,book.name,book.price,book.description,
-            WriterDto(book.writer.id,book.writer.nome,book.writer.nationality)) }.toList()
+        fun listBookToListBookDto(book: Maybe<List<Book>>) = book.map {bookList ->
+            bookList.stream().map { book -> BookDto(book.id,book.name,book.price,book.description,
+                WriterConverter.writerToWriterDto(
+                    Writer(book.writer.id,book.writer.nome,book.writer.nationality)))}.toList() }
 
-    fun listBookDtoToListBook(bookDto: List<BookDto>) =
-        bookDto.stream().map { bookDto -> Book(bookDto.id, bookDto.name, bookDto.price, bookDto.description,
-        Writer(bookDto.writer.id, bookDto.writer.nome, bookDto.writer.nationality))}.toList()
+        fun listBookDtoToListBook(bookDto: Maybe<List<BookDto>>) = bookDto.map {bookDtoList ->
+            bookDtoList.stream().map { bookDto ->
+                Book(bookDto.id, bookDto.name, bookDto.price, bookDto.description,
+                WriterConverter.writerDtoToWriter(
+                    WriterDto(bookDto.writer.id, bookDto.writer.nome, bookDto.writer.nationality)))}.toList() }
+    }
 }
