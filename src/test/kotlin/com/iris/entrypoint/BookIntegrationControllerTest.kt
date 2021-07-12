b7ce2d190ca917146e57f361020311c6b1449f01
Iris <iris.souza@zup.com.br>
2021-07-12 18:09:29 -0300

    Adicionar teste unitário de criar um livro na BookIntegrationControllerTest

---
+++ src/test/kotlin/com/iris/entrypoint/BookIntegrationControllerTest.kt
@@ -0,0 +1,47 @@
+package com.iris.entrypoint
+
+import com.iris.core.mapper.BookConverter
+import com.iris.core.port.BookIntegrationPort
+import com.iris.entrypoint.controller.integration.BookIntegrationController
+import com.iris.entrypoint.dto.BookDto
+import com.iris.entrypoint.dto.WriterDto
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.http.HttpStatus
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Maybe
+import java.math.BigDecimal
+
+@MicronautTest
+class BookIntegrationControllerTest : AnnotationSpec() {
+
+    val bookIntegrationPort = mockk<BookIntegrationPort>()
+    val bookIntegration = BookIntegrationController(bookIntegrationPort)
+
+    lateinit var bookDto: BookDto
+    lateinit var listOfBookDto: List<BookDto>
+    lateinit var maybeListOfBookDto: Maybe<List<BookDto>>
+    lateinit var maybeBookDto: Maybe<BookDto>
+
+    @BeforeEach
+    fun setUp() {
+        bookDto = BookDto(1L, "It ends with us", BigDecimal.ONE, "Its a romance about toxic relationship",
+            WriterDto(1L, "It ends with us", "American")
+        )
+
+        listOfBookDto = listOf(bookDto)
+        maybeListOfBookDto = Maybe.just(listOfBookDto)
+        maybeBookDto = Maybe.just(bookDto)
+    }
+
+    @Test
+    fun `should create a book`() {
+        every { BookConverter.bookToBookDto(bookIntegrationPort.createBook(any())) } returns maybeBookDto
+
+        val result = bookIntegration.postBook(bookDto)
+
+        result.status shouldBe HttpStatus.CREATED
+    }
+}
