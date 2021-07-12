4e8098664c932a1ae461713b086e03d8c0e2bcba
Iris <iris.souza@zup.com.br>
2021-07-12 15:50:40 -0300

    Adicionar método post na classe de Low Level Client

---
+++ src/main/kotlin/com/iris/entrypoint/httpclient/CockroachApiLowLevelClient.kt
@@ -0,0 +1,25 @@
+package com.iris.entrypoint.httpclient
+
+import com.iris.entrypoint.dto.BookDto
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpRequest.POST
+import io.micronaut.http.HttpRequest.PUT
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.uri.UriBuilder
+import io.reactivex.Maybe
+import java.net.URI
+import javax.inject.Singleton
+
+@Singleton
+class CockroachApiLowLevelClient
+    (@param:Client("http://localhost:8081") private val httpClient: RxHttpClient){
+
+    fun saveBook(bookDto: BookDto): Maybe<BookDto> {
+        val uri: URI = UriBuilder.of("/v1/iupp/book/").build()
+        val req: HttpRequest<*> = POST<Any>(uri, bookDto).body(bookDto)
+        val flowable = httpClient.retrieve(req, Argument.of(BookDto::class.java))
+        return flowable.firstElement()
+    }
+}
