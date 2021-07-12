6a7c9c13f1cc340b5075e67e6d9d0fbca881636f
Iris <iris.souza@zup.com.br>
2021-07-12 17:12:30 -0300

    Implementar método de atualizar livro na BookIntegrationController

--- src/main/kotlin/com/iris/entrypoint/controller/integration/BookIntegrationController.kt
+++ src/main/kotlin/com/iris/entrypoint/controller/integration/BookIntegrationController.kt
@@ -1,9 +1,9 @@
 package com.iris.entrypoint.controller.integration
 
 import com.iris.core.mapper.BookConverter
-import com.iris.core.model.Book
 import com.iris.core.port.BookIntegrationPort
 import com.iris.entrypoint.dto.BookDto
+import com.iris.entrypoint.dto.WriterDto
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
@@ -25,6 +25,14 @@ class BookIntegrationController(private val bookIntegrationPort: BookIntegration
     fun getAllBooks(): MutableHttpResponse<Maybe<List<BookDto>>> {
         return HttpResponse.ok(BookConverter.listBookToListBookDto(
             bookIntegrationPort.findAllBooks()))
+    }
+
+    @Put("{id}/")
+    fun updateBook(id: Long, @Body book: BookDto) : MutableHttpResponse<Maybe<BookDto>> {
+        val bookDto = BookDto(id, book.name, book.price, book.description,
+            WriterDto(null, book.writer.nome, book.writer.nationality),
+        )
+        return HttpResponse.ok(BookConverter.bookToBookDto(bookIntegrationPort.updateBook(id, bookDto)))
     }
 
     @Delete("/{id}")
